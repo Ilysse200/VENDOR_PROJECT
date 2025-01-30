e1db2b03217f7619fa4bb3453b40e8ece9b3fa0c
Ilysse200 <ilysseted500@gmail.com>
2025-01-30 22:28:01 +0200

    added images to the vendor page

--- vendorPlatform/src/components/Vendor.jsx
+++ vendorPlatform/src/components/Vendor.jsx
@@ -1,50 +1,55 @@
 import React from "react";
 import { FaStar, FaFilter, FaThLarge, FaList } from "react-icons/fa";
 import "../styles/vendor.css";
-
+import outfitBlack from '../assets/jeans.webp';
+import green from '../assets/greenShoes.webp';
+import glasses from '../assets/glasses.webp';
+import equipment from '../assets/blackEquipment.webp';
+import person from '../assets/blueShirt.webp';
+import blankPerson from '../assets/blankProfile.png'
 const stores = [
   {
     name: "John Doe's Store",
     image: "https://demo.athemes.com/botiga-multi-vendor-el/wp-content/webp-express/webp-images/doc-root/wp-content/uploads/sites/145/2022/12/cropped-vendor-banner-6.jpg.webp",
     location: "Central Park, New York, United States (US)",
     rating: 4.5,
-    profilePic: "https://demo.athemes.com/botiga-multi-vendor-el/store/john-does-store-2/",
+    profilePic: glasses,
   },
   {
     name: "Jessica's Store",
     image: "https://demo.athemes.com/botiga-multi-vendor-el/wp-content/webp-express/webp-images/doc-root/wp-content/uploads/sites/145/2022/12/cropped-vendor-shoes-shop-1-2.jpg.webp",
     location: "Central Park, New York, United States (US)",
     rating: 4.0,
-    profilePic: "https://via.placeholder.com/50",
+    profilePic: green,
   },
   {
     name: "Santa Monica's Store",
     image: "https://demo.athemes.com/botiga-multi-vendor-el/wp-content/webp-express/webp-images/doc-root/wp-content/uploads/sites/145/2022/12/cropped-vendor-banner-1-1.jpg.webp",
     location: "Central Park, New York, United States (US)",
     rating: 5.0,
-    profilePic: "https://via.placeholder.com/50",
+    profilePic: outfitBlack,
   },
   {
     name: "Digital Good's Store",
     image: "https://demo.athemes.com/botiga-multi-vendor-el/wp-content/webp-express/webp-images/doc-root/wp-content/uploads/sites/145/2022/12/cropped-vendor-banner-4-2.jpg.webp",
     location: "Central Park, New York, United States (US)",
     rating: 4.0,
-    profilePic: "https://via.placeholder.com/50",
+    profilePic: equipment,
   },
   {
     name: "The Glass Store",
     image: "https://demo.athemes.com/botiga-multi-vendor-el/wp-content/webp-express/webp-images/doc-root/wp-content/uploads/sites/145/2022/12/cropped-vendor-banner-5-1.jpg.webp",
     location: "Central Park, New York, United States (US)",
     rating: 4.7,
-    profilePic: "https://via.placeholder.com/50",
+    profilePic: person,
   },
   {
     name: "Josh Doe's Store",
     image: "https://demo.athemes.com/botiga-multi-vendor-el/wp-content/uploads/sites/145/2022/12/vendor-banner-3.jpg",
     location: "Central Park, New York, United States (US)",
     rating: 4.8,
-    profilePic: "https://via.placeholder.com/50",
-  },
+    profilePic:blankPerson,
+  }
 ];
 
 const StoreList = () => {
