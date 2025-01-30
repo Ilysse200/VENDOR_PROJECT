d9f95d0e84dacf3d97ec69e5cbda93aa1cb28b25
Ilysse200 <ilysseted500@gmail.com>
2025-01-30 12:20:19 +0200

    finalized homePage

--- vendorPlatform/src/components/Content.jsx
+++ vendorPlatform/src/components/Content.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import '../styles/content.css'
+import { FaStar, FaStarHalfAlt, FaRegStar } from "react-icons/fa";
+
 
 //  Import images correctly
 import lotion from '../assets/lotion.webp';
@@ -8,6 +10,10 @@ import product3 from '../assets/product-category-3.jpg.webp';
 import product8 from '../assets/product-category-8-1.jpg.webp';
 import product9 from '../assets/product-category-9.jpg.webp';
 import product10 from '../assets/product-category-10.jpg.webp';
+
+import image3 from '../assets/product-wireless-headset-420x420.jpg (1).webp';
+import image1 from '../assets/product-water-bottle-420x420.jpg.webp';
+import image2 from '../assets/alexa.webp';
 
 //  Use imported images in the array
 const categories = [
@@ -18,6 +24,32 @@ const categories = [
     { id: 5, name: "Watch", image: product9 },
     { id: 6, name: "Woman Clothes", image: product10 }
 ];
+const products =[
+    {
+        name: "bottle",
+        picture:image1,
+        price:"$22.00 - $55.00",
+        rating: [<FaStar color='orange'/>, <FaStar color='orange'/>, <FaStar color='orange'/>, <FaStar color='grey'/>,, <FaStar color='grey'/>
+    ]
+
+    }, 
+    {
+        name: "Windows Alexa",
+        picture:image2,
+        price:"$49.00 – $69.00",
+        rating: [<FaStar color='orange'/>, <FaStar color='orange'/>, <FaStar color='orange'/>, , <FaStar color='orange'/>,, <FaStar color='orange'/>
+    ]
+
+    },
+    {
+        name: "HeadPhones",
+        picture:image3,
+        price:"$22.00 - $55.00",
+        rating: [<FaStar color='orange'/>, <FaStar color='orange'/>, <FaStar color='orange'/>, , <FaStar color='orange'/>, <FaStar color='grey'/>
+    ]
+
+    }
+]
 
 export default function Content() {
   return (
@@ -31,6 +63,23 @@ export default function Content() {
                 </div>
             ))}
         </div>
+        <h1 className='productTitle'>New Arrival Products</h1>
+        <div className='productsClass'>
+            {
+                products.map((product) => (
+                    <div key={product.name} className='product'>
+                        <img src={product.picture} alt={product.name} className='productImage'/>
+                        <h2>{product.name}</h2>
+                        <p>{product.price}</p>
+                        <div className='rating'>
+                            {product.rating.map((star) => (star))}
+                        </div>
+                    </div>
+                ))
+            }
+        </div>
+        <button className='shop-Button'>SHOP NOW</button>
+        
     </div>
   );
 }
